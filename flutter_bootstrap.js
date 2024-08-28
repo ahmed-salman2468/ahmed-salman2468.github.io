2b194e055b6722c9a38811913a839e4988ab9f5a
ahmed-salman2468 <nodesalman2468@gmail.com>
2024-08-28 23:16:34 +0500

    version 1.0.28

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2195860296"
+    serviceWorkerVersion: "1036236406"
   }
 });
